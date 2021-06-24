807784b96df8c3d8f9337ee848402165e15a40e9
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-06-24 17:05:05 -0300

    add authot path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,22 @@
 const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
 // add collection field in order to filter by authors, pages nodes
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'MarkdownRemark') {
-    const contentInstaceName = getNode(node?.parent)?.sourceInstanceName // authors,pages...
+    const contentInstaceName = getNode(node.parent).sourceInstanceName // authors,pages...
     createNodeField({
       name: 'collection',
       node,
       value: contentInstaceName
+    })
+    createNodeField({
+      name: 'slug',
+      node,
+      value: createFilePath({
+        node, getNode
+      })
     })
   }
 }
@@ -35,6 +43,9 @@ exports.createPages = async ({ graphql, actions }) => {
             frontmatter {
               title
             }
+            fields {
+              slug
+            }
           }
         }
       }
@@ -42,32 +53,32 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
 
   const templatePost = path.resolve('src/templates/post.js')
-  posts?.data?.posts.edges.map(post => {
+  posts.data.posts.edges.map(post => {
     // console.log('posts', JSON.stringify(post, null, 2))
     createPage({
-      path: post?.node?.frontmatter?.path,
+      path: post.node.frontmatter.path,
       component: templatePost,
       context: {
-        id: post?.node?.frontmatter?.path,
+        id: post.node.frontmatter.path,
       }
     })
   })
 
   const templateAuthor = path.resolve('src/templates/author.js')
-  posts?.data?.authors.edges.map(author => {
+  posts.data.authors.edges.map(author => {
     console.log('author', JSON.stringify(author, null, 2))
     createPage({
-      path: author?.node?.frontmatter?.title,
+      path: author.node.fields.slug,
       component: templateAuthor,
       context: {
-        id: author?.node?.frontmatter?.title,
+        id: author.node.fields.slug,
       }
     })
   })
 
   // pagination
   const pageSize = 2
-  const totalPosts = posts?.data?.posts?.edges?.length
+  const totalPosts = posts.data.posts.edges.length
   const numPages = Math.ceil(totalPosts / pageSize)
   const templateBlog = path.resolve('src/templates/blog.js')
   // console.log('numPages', numPages)
@@ -75,9 +86,9 @@ exports.createPages = async ({ graphql, actions }) => {
   Array
     .from({ length: numPages })
     .forEach((_, i) => {
-      console.log('gen', '/blog' + (i == 0 ? '' : `/${i}`))
+      console.log('gen', '/blog' + (i == 0  '' : `/${i}`))
       createPage({
-        path: '/blog' + (i == 0 ? '' : `/${i}`),
+        path: '/blog' + (i == 0  '' : `/${i}`),
         component: templateBlog,
         context: {
           limit: pageSize,
